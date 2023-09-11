0a3bda6e3ba2accf2d13d2b3281f595cee66221d
Pascal Pitette <ppitette@gmail.com>
2023-09-11 13:12:37 +0200

    📦 NEW: Mise en place d'une authentification

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\CreateAdminService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Création d\'un administrateur',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private readonly createAdminService $createAdminService,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::OPTIONAL, 'Adresse électronique de l\'administrateur')
+            ->addArgument('password', null, InputOption::VALUE_NONE, 'Mot de passe de l\'administrateur')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $this->createAdminService->create($email, $password);
+
+        $io->success('L\'Administrateur a bien été créé');
+
+        return Command::SUCCESS;
+    }
+}
