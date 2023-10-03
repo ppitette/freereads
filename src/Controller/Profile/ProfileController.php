41c6baf0230ad51c72f96babce838960cf09306d
Pascal Pitette <ppitette@gmail.com>
2023-10-03 18:15:38 +0200

    📦 NEW: Recherche de livres avec l'API Google Book

--- src/Controller/Profile/ProfileController.php
+++ src/Controller/Profile/ProfileController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller\Profile;
 
+use App\Service\GoogleBooksApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/profile', name: 'app_profile_')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    public function __construct(
+        private readonly GoogleBooksApiService $googleBooksApiService
+    ) {
+    }
+
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('profile/profile/index.html.twig', [
+        return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+        ]);
+    }
+
+    #[Route('/search', name: 'search')]
+    public function search(): Response
+    {
+        return $this->render('profile/search.html.twig');
+    }
+
+    #[Route('/search/api', name: 'search_api', methods: ['POST'])]
+    public function searchApi(Request $request): Response
+    {
+        $search = $request->request->get('search');
+
+        return $this->render('profile/_api.html.twig', [
+            'search' => $this->googleBooksApiService->search($search),
         ]);
     }
 }
---
+++ src/Service/GoogleBooksApiService.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GoogleBooksApiService
+{
+    public function __construct(
+        private readonly HttpClientInterface $googlebooksClient
+    ) {
+    }
+
+    public function search(string $search): array
+    {
+        if (strlen($search) < 3) {
+            return [];
+        }
+
+        return $this->makeRequest('GET', 'volumes', [
+            'query' => [
+                'q' => $search,
+            ],
+        ]);
+    }
+
+    private function makeRequest(string $method, string $url, array $options = []): array
+    {
+        $response = $this->googlebooksClient->request($method, $url, $options);
+
+        return $response->toArray();
+    }
+}
